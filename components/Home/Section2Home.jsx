1421266d2158c57bfecc4adf931b64652f997fa2
Kaenova Mahendra Auditama <kaenova@gmail.com>
2021-12-12 22:44:56 +0700

    Animating on What I Do

--- components/Home/Section2Home.jsx
+++ components/Home/Section2Home.jsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react'
-import { motion, AnimatePresence } from "framer-motion";
+import React, { useEffect, useState } from "react";
 import { useWindowWidth } from "@react-hook/window-size";
-
+import { motion, AnimatePresence, AnimateSharedLayout } from "framer-motion";
 
 function Section2Home() {
   const tailwindBreakpoint = {
@@ -12,15 +11,52 @@ function Section2Home() {
     "2xl": 1536,
     // All in pixels
   };
+  const whatIDoVerb = [
+    "Data Engineering",
+    "Frontend",
+    "Backend",
+    "DevOps Engineering",
+  ];
 
-  const [ModalIsActive, setModalIsActive] = useState(false)
+  const [ModalIsActive, setModalIsActive] = useState(false);
   const windowWidth = useWindowWidth();
+  const [selected, setSelected] = useState(whatIDoVerb[0]);
+
   useEffect(() => {
     console.log(windowWidth);
     if (windowWidth > tailwindBreakpoint["sm"]) {
       setModalIsActive(false);
     }
   }, [windowWidth]);
+
+  const WhatIDoItem = ({ nama, isSelected, onClick }) => {
+    return (
+      <>
+        <button onClick={onClick} className="relative flex flex-col justify-center items-center">
+          {isSelected ? (
+            <>
+              <motion.p className="text-[14px] font-bold z-[1]"
+              animate={{color:"#F7F7F7"}}
+              >{nama}</motion.p>
+              <motion.div
+                className="text-[14px] font-bold absolute"
+                animate={{
+                  color: "#F7F7F7",
+                  backgroundColor: "#353535",
+                  height: "35px",
+                  borderRadius: "9px",
+                  width: "120%"
+                }}
+              >
+              </motion.div>
+            </>
+          ) : (
+            <motion.p className="text-[14px] font-bold">{nama}</motion.p>
+          )}
+        </button>
+      </>
+    );
+  };
 
   return (
     <>
@@ -57,65 +93,109 @@ function Section2Home() {
           </motion.div>
         )}
       </AnimatePresence>
-      <section id="idx_sec2" className="h-full relative flex flex-col justify-center items-center pl-[50px] pr-[50px]">
-        <h1 className="text-[24px] font-bold tracking-normal">
-          What I Do?
-        </h1>
-        <div className=' mt-[24px] '>
+      <section
+        id="idx_sec2"
+        className="h-full relative flex flex-col justify-center items-center pl-[50px] pr-[50px]"
+      >
+        <h1 className="text-[24px] font-bold tracking-normal">What I Do?</h1>
+        <div className=" mt-[24px] ">
           {/* TODO: ONCLICK MODAL HANDLER */}
-          <button onClick={() => setModalIsActive(true)} className="bg-white rounded-[7px] shadow-md min-w-[306px] max-w-[420px] min-h-[43px] flex flex-row flex-grow justify-between items-center pr-[14px] select-none sm:hidden">
+          <button
+            onClick={() => setModalIsActive(true)}
+            className="bg-white rounded-[7px] shadow-md min-w-[306px] max-w-[420px] min-h-[43px] flex flex-row flex-grow justify-between items-center pr-[14px] select-none sm:hidden"
+          >
             <p className="font-bold text-[14px] tracking-normal ml-[40px]">
               Data Engineering
             </p>
-            <img className="w-[24px] h-[24px]" src="/menu-outline.svg" alt="" srcSet='' />
+            <img
+              className="w-[24px] h-[24px]"
+              src="/menu-outline.svg"
+              alt=""
+              srcSet=""
+            />
           </button>
           {/* TODO: Bagian Selection To Do */}
-          <div className='hidden sm:block'>
-            <div className='w-[772px] h-[42px] bg-white shadow-md rounded-[7px] flex flex-row justify-between pl-[34px] pr-[34px]'>
-              <button className='text-[14px] font-bold'>Data Engineering</button>
-              <button className='text-[14px] font-bold'>Frontend</button>
-              <button className='text-[14px] font-bold'>Backend</button>
-              <button className='text-[14px] font-bold'>DevOps Engineer</button>
-            </div>
+          <div className="hidden sm:block">
+            <AnimateSharedLayout>
+              <div className="w-[772px] h-[42px] bg-white shadow-md rounded-[7px] flex flex-row justify-between pl-[20px] pr-[20px] items-center">
+                {whatIDoVerb.map((v) => (
+                  <WhatIDoItem
+                    key={v}
+                    nama={v}
+                    isSelected={selected === v}
+                    onClick={() => setSelected(v)}
+                  />
+                ))}
+                {/* <button className="text-[14px] font-bold block_what_i_do">
+                  Frontend
+                </button>
+                <button className="text-[14px] font-bold block_what_i_do">
+                  Backend
+                </button>
+                <button className="text-[14px] font-bold block_what_i_do">
+                  DevOps Engineer
+                </button> */}
+              </div>
+            </AnimateSharedLayout>
           </div>
         </div>
-        <span className='mt-[25px]' />
-        <div className='sm:grid sm:grid-cols-2 sm:gap-[75px] sm:items-center'>
-          <div className='hidden sm:block sm:bg-red-500'> {/* Ini Bagian Kanan */}
-            <div className='w-full h-full bg-blue-500'>
+        <span className="mt-[25px]" />
+        <div className="sm:grid sm:grid-cols-2 sm:gap-[75px] sm:items-center">
+          <div className="hidden sm:block sm:bg-red-500">
+            {" "}
+            {/* Ini Bagian Kanan */}
+            <div className="w-full h-full bg-blue-500">
               <img src="https://picsum.photos/seed/picsum/200/300" alt="" />
             </div>
           </div>
-          <div className="max-w-[420px]"> {/* Ini Bagian Kanan */}
-            <h2 className='text-[24px] font-bold mb-[15px] hidden sm:block'>Data Engineering</h2>
+          <div className="max-w-[420px]">
+            {" "}
+            {/* Ini Bagian Kanan */}
+            <h2 className="text-[24px] font-bold mb-[15px] hidden sm:block">
+              Data Engineering
+            </h2>
             <p className="tracking-normal text-justify">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia inventore corrupti earum nisi totam labore consequuntur expedita voluptatum eaque natus vitae nemo nulla at voluptate quis, adipisci facilis, repellat ducimus? Ini maksimal
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+              inventore corrupti earum nisi totam labore consequuntur expedita
+              voluptatum eaque natus vitae nemo nulla at voluptate quis,
+              adipisci facilis, repellat ducimus? Ini maksimal
             </p>
             <div className="mt-[20px] flex flex-col gap-3 justify-center items-center sm:text-left sm:items-start">
               <h2 className="tracking-heading font-bold text-center text-[24px] sm:text-left">
                 Top Projects
               </h2>
-              <a className="underline tracking-normal" href="">Project 1</a>
-              <a className="underline tracking-normal" href="">Project 2</a>
+              <a className="underline tracking-normal" href="">
+                Project 1
+              </a>
+              <a className="underline tracking-normal" href="">
+                Project 2
+              </a>
             </div>
           </div>
         </div>
         <div className="absolute bottom-[20px] w-full flex flex-col justify-center">
-          <button onClick={() => document.getElementById("idx_sec2").scrollIntoView()} className="flex flex-col justify-center items-center select-none">
-            <motion.div
-              whileHover={{ scale: 1.10 }}
-              whileTap={{ scale: 0.90 }}
-            >
+          <button
+            onClick={() => document.getElementById("idx_sec2").scrollIntoView()}
+            className="flex flex-col justify-center items-center select-none"
+          >
+            <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
               <motion.img
                 animate={{ opacity: 0.1 }}
-                transition={{ duration: 2, repeat: Infinity, repeatType: "mirror" }}
-                className="w-[47px] h-[47px] mt-[10px]" src="/arrow-ios-downward-outline.svg" alt="" />
+                transition={{
+                  duration: 2,
+                  repeat: Infinity,
+                  repeatType: "mirror",
+                }}
+                className="w-[47px] h-[47px] mt-[10px]"
+                src="/arrow-ios-downward-outline.svg"
+                alt=""
+              />
             </motion.div>
           </button>
         </div>
       </section>
     </>
-  )
+  );
 }
 
-export default Section2Home
+export default Section2Home;
