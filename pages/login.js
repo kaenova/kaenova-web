b2a6788177bf73cd1db008c91a4f234141b90d5a
Kaenova Mahendra Auditama <kaenova@gmail.com>
2022-01-24 22:05:36 +0700

    Adding SEO

--- pages/login.js
+++ pages/login.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import Login from '../components/Login/Login'
+import { NextSeo } from 'next-seo'
 
 function login() {
   
   return (
     <>
+    <NextSeo
+        title="KMA"
+        description="Kaenova's portfolio and personal platform. Kaneova is a Software Engineer and Data Engineer studying in Telkom University. He likes to explore new technology."
+        openGraph={
+          {
+            title: "KMA: Homepage",
+            description: "Kaenova's portfolio and personal platform. Kaneova is a Software Engineer and Data Engineer studying in Telkom University. He likes to explore new technology.",
+            url: "https://kaenova.my.id",
+            images: [{url: "https://kaenova.my.id/SEO.png"}],
+            site_name: "KMA"
+          }
+        }
+      />
       <Login/>
     </>
   )
