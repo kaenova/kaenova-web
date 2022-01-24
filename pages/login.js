5e2aec0fb17034f1a203a0379b6ff28adafc1d1f
Kaenova Mahendra Auditama <kaenova@gmail.com>
2022-01-24 22:09:20 +0700

    Changing SEO

--- pages/login.js
+++ pages/login.js
@@ -7,11 +7,11 @@ function login() {
   return (
     <>
     <NextSeo
-        title="KMA"
+        title="KMA: Login"
         description="Kaenova's portfolio and personal platform. Kaneova is a Software Engineer and Data Engineer studying in Telkom University. He likes to explore new technology."
         openGraph={
           {
-            title: "KMA: Homepage",
+            title: "KMA: Login Page",
             description: "Kaenova's portfolio and personal platform. Kaneova is a Software Engineer and Data Engineer studying in Telkom University. He likes to explore new technology.",
             url: "https://kaenova.my.id",
             images: [{url: "https://kaenova.my.id/SEO.png"}],
