89accc624fa654ff5aea1bc2ef47700a5723a4ef
Kaenova Mahendra Auditama <kaenova@gmail.com>
2022-08-24 10:03:09 +0700

    using animatepresence

--- components/pages/loading.tsx
+++ components/pages/loading.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import LimitSizeLayout from "../layout/limit_size";
 import MainLayout from "../layout/main_layout";
 import NormalPadding from "../layout/normal_padding";
@@ -14,43 +14,50 @@ type LoadingProps = {
 };
 
 function Loading({ percentages, isDone }: LoadingProps) {
-
   const [Remove, setRemove] = useState(false);
-  
+
   useEffect(() => {
     if (isDone) {
-      setTimeout(() => {
-        setRemove(true);
-      }, 2000);
+      setRemove(true);
     }
   }, [isDone]);
 
   return (
-    <motion.div
-      className={"fixed z-10 w-full transition-all ease-in-out"}
-      animate={{
-        y: Remove ? "-100rem" : "0",
-        opacity: Remove? 0: 100
-      }}
-    >
-      <MainLayout>
-        <LimitSizeLayout>
-          <NormalPadding>
-            <div className="h-screen flex flex-col justify-center items-center">
-              <span className="flex flex-col justify-center items-center gap-[-30px]">
-                <Brand className="text-center" />
-                <div className="w-[50px] h-[3px] bg-accent animate-pulse"></div>
-              </span>
-              <span className="flex flex-row transition-all animate-pulse">
-                <H1Fill>Load</H1Fill>
-                <H1Outline>ing</H1Outline>
-              </span>
-              <H3Fill>{percentages}%</H3Fill>
-            </div>
-          </NormalPadding>
-        </LimitSizeLayout>
-      </MainLayout>
-    </motion.div>
+    <AnimatePresence>
+      {!Remove && (
+        <motion.div
+          className={"fixed z-10 w-full transition-all ease-in-out"}
+          animate={{
+            opacity: 1,
+          }}
+          exit={{
+            y: "-100vh",
+            opacity: 0,
+          }}
+          transition={{ 
+            delay: 2,
+           }}
+        >
+          <MainLayout>
+            <LimitSizeLayout>
+              <NormalPadding>
+                <div className="h-screen flex flex-col justify-center items-center">
+                  <span className="flex flex-col justify-center items-center gap-[-30px]">
+                    <Brand className="text-center" />
+                    <div className="w-[50px] h-[3px] bg-accent animate-pulse"></div>
+                  </span>
+                  <span className="flex flex-row transition-all animate-pulse">
+                    <H1Fill>Load</H1Fill>
+                    <H1Outline>ing</H1Outline>
+                  </span>
+                  <H3Fill>{percentages}%</H3Fill>
+                </div>
+              </NormalPadding>
+            </LimitSizeLayout>
+          </MainLayout>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 }
 
