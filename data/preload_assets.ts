a0e3f48622cec0a3ba4f803c20dc42bf5892b5e7
Kaenova Mahendra Auditama <kaenova@gmail.com>
2022-08-25 15:17:01 +0700

    adding audio on coin

--- data/preload_assets.ts
+++ data/preload_assets.ts
@@ -14,7 +14,8 @@ export const preloadImage = [
 ]
 
 export const preloadPixiAssets = [
-  {key: "coin", url: "https://art.pixilart.com/0433de3a9dca4b9.png"},
-  {key: "box", url: "/box.png"},
-  {key: "flashlight", url:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/White_Circle.svg/2048px-White_Circle.svg.png"}
+  { key: "coin", url: "https://art.pixilart.com/0433de3a9dca4b9.png" },
+  { key: "box", url: "/box.png" },
+  { key: "flashlight", url: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/White_Circle.svg/2048px-White_Circle.svg.png" },
+  { key: "coin_sound", url: "/coin.mp3" }
 ]    
