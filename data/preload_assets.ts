4574afdd0e7e30f4adcd189fcf21b9a0eede0ef5
Kaenova Mahendra Auditama <61568092+kaenova@users.noreply.github.com>
2022-09-23 12:47:13 +0700

    Moving all external assets to internal (#14)
    * feat(data): change all assets to internal
    
    * feat(data): change all ref

--- data/preload_assets.ts
+++ data/preload_assets.ts
@@ -1,6 +1,6 @@
 export const preloadImage = [
-  "https://art.pixilart.com/0433de3a9dca4b9.png",
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/White_Circle.svg/2048px-White_Circle.svg.png",
+  "/coin.png",
+  "/white-circle.png",
   "/kerjago_mono.png",
   "/ailab_mono.png",
   "/bangkit_mono.png",
@@ -14,15 +14,15 @@ export const preloadImage = [
   "/travelliu.png",
   "/hotelbli.png",
   "/wow.gif", 
-  "https://c.tenor.com/MhVJtrDqT8IAAAAd/nice-thumbs-up.gif", 
-  "https://c.tenor.com/UniGtspR-BcAAAAC/damage-thats-a-lot-of-damage.gif", 
-  "https://64.media.tumblr.com/36b6133796e2f93ae88c282b23621da2/05bd0b5ff8579b22-cc/s540x810/c6679aeecffb451e20afaa6c6c62bf625eeb39c6.gif"
+  "/thumbs.gif", 
+  "/lots-of-damage.gif", 
+  "/getting-paid.gif",
 ]
 
 export const preloadPixiAssets = [
-  { key: "coin", url: "https://art.pixilart.com/0433de3a9dca4b9.png" },
+  { key: "coin", url: "/coin.png" },
   { key: "box", url: "/box.png" },
-  { key: "flashlight", url: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/White_Circle.svg/2048px-White_Circle.svg.png" },
+  { key: "flashlight", url: "/white-circle.png" },
   { key: "coin_sound", url: "/coin.mp3" },
   { key: "lofi_bgm", url: "/lofi.mp3" }
 ]    
