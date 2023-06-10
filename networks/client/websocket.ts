5e0087725e5d4f705199cab3a25db78967a76066
Kaenova Mahendra Auditama <kaenova@gmail.com>
2023-06-10 18:31:32 +0700

    starting clean

--- networks/client/websocket.ts
+++
@@ -1,6 +0,0 @@
-
-export function webSocketInstance(path: string): WebSocket {
-  let endpoint = `${process.env.NEXT_PUBLIC_KAENOVA_WEBSOCKET_ENDPOINT}${path}`
-  var ws = new WebSocket(endpoint)
-  return ws
-}
