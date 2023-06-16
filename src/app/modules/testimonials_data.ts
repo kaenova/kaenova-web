16d8c6434dcb9ce07d323001cb61b6ce742e5a29
Kaenova Mahendra Auditama <61568092+kaenova@users.noreply.github.com>
2023-06-16 18:26:51 +0700

    Update testimonials_data.ts

--- src/app/modules/testimonials_data.ts
+++ src/app/modules/testimonials_data.ts
@@ -5,96 +5,96 @@ export interface Testimonials {
 
 const testimonials = [
   {
-    name: "I Komang Tri Cahya Bramantara",
-    review: "Very nice explanation Kak Kae, you are so polite and kind.",
+    name: "Komang Tri Cahya Bramantara",
+    review: "Very nice explanation, polite and kind.",
   },
   {
     name: "Andre Chandra",
-    review: "The mentor is excellent and has a strong grasp of the technical aspects. They provide constructive advice and are willing to immerse themselves in observing the issues directly.",
+    review: "Excellent mentor with strong technical grasp.",
   },
   {
     name: "Fikriyah Istiqomah",
-    review: "Beneran lu keren banget Bang Kae! Cara bicara abang yang santai, mudah dipahami, dan sangat berbobot. Makasih banyak udah ngasih saran yang sangat membangun dan ngasih apresiasi juga. Kerenn abang mau share skill yang udah expert banget ke kami yang masih pemula. Sukses terus ya!",
+    review: "Keren banget! Santai, mudah dipahami, dan berbobot. Terima kasih banyak!",
   },
   {
     name: "Aleisya Zahari Salam",
-    review: "Mentoring activities are smooth, not too strict and friendly. The mentor provides constructive advice for the project's development. Glad attending this mentoring, gained many insights. Keep encouraging achievements and trying. Thank you.",
+    review: "Smooth mentoring, constructive advice, and many helpful insights.",
   },
   {
     name: "Fikri Yoma Rosyidan",
-    review: "Pemahamaan integrasi arsitektur CC dan ML kak Kae sangat informatif",
+    review: "Informative mentoring on architecture integration.",
   },
   {
     name: "Galang Rambu Ramadhan",
-    review: "Kak Kae is so helpful, solving our problem in ModelML and teaching us how to use the model in Backend.",
+    review: "Very helpful in solving ModelML problems.",
   },
   {
     name: "Dewa Putra Hernanda",
-    review: "The mentoring is useful, providing on-point and easy-to-understand explanations and answers for my problems.",
+    review: "On-point explanation, easy to understand.",
   },
   {
     name: "Luswis Arya",
-    review: "Sir Kae answered all my questions clearly and provided fast responses when scheduling appointments. Easy to understand and coordinate with. Impressive mentoring experience.",
+    review: "Clear answers, easy scheduling, and fast responses.",
   },
   {
     name: "Farkhan Maulana",
-    review: "During our mentoring session on Backend API development, Kak Kae showed strong technical knowledge, explaining complex concepts clearly. Providing guidance and support to improve skills. Suggestion: Providing more examples and real-world use cases.",
+    review: "Strong technical knowledge, clear explanations, and helpful guidance.",
   },
   {
     name: "Muhtarom Ahkam",
-    review: "Bagus banget, mentornya sangat tersusun menjelaskan sesuatu, dan juga cara pembawaanya tidak seperti guru dan murid tapi kayak teman",
+    review: "Tersusun menjelaskan sesuatu, cara pembawaan yang santai.",
   },
   {
     name: "Ellion Blessan",
-    review: "The mentoring session was comfortable and very insightful. 1-on-1 session truly helps. Pleasant and friendly conversation.",
+    review: "Comfortable and insightful mentoring sessions.",
   },
   {
     name: "Abdullah Al Masyhur",
-    review: "Humble, friendly, and knowledgeable mentor. Provides direct solutions, shares experiences and references generously. Valuable short mentoring time. Thank you!",
+    review: "Humble, friendly, knowledgeable mentor. Provides direct solutions and valuable experience.",
   },
   {
     name: "Jasica Ardana Herviyandasari",
-    review: "Nothing bad at all. Mentoring with Kak Kaenova is very helpful, providing real solutions. Friendly and supportive. Thank you!",
+    review: "Very helpful, real solutions, and friendly.",
   },
   {
     name: "Nalendra Kayana",
-    review: "Kak Kae has been extremely helpful during machine learning mentoring, providing valuable suggestions and ideas.",
+    review: "Extremely helpful with machine learning issues.",
   },
   {
     name: "Muhammad Faadhil Putra Wahana",
-    review: "Super satisfied and helpful. Resolved all my problems and gave a lot of improvements to my app.",
+    review: "Super satisfied and helpful. Improved my app.",
   },
   {
     name: "Laurensia Simanihuruk",
-    review: "During mentoring, my mentor provides good insights and solutions, creating a comfortable atmosphere.",
+    review: "Provides good insights and comfortable mentoring atmosphere.",
   },
   {
     name: "ml cohort",
-    review: "Very helpful for those of us who are so confused.",
+    review: "Very helpful for confused learners.",
   },
   {
     name: "fadhila f",
-    review: "Kak Kae really helps us step by step and detailed. Humble as well.",
+    review: "Helps step by step and detailed. Humble mentor.",
   },
   {
     name: "Azizah Fathimatuzzahro",
-    review: "I'm satisfied doing mentoring with Kak Kaenova. Resolved our problems properly.",
+    review: "Resolved problems properly, satisfied with the mentoring.",
   },
   {
     name: "Zain Zaidan Ahsan",
-    review: "Sangat terbantu dan sangat bagus",
+    review: "Terbantu dan sangat bagus.",
   },
   {
     name: "Indriani Astuti",
-    review: "I really like the way he teaches me, he teaches me in detail until I really understand. Nice and relaxed.",
+    review: "Detailed teaching, nice and relaxed atmosphere.",
   },
   {
     name: "Yunike Warda Nabila",
-    review: "Seruuu dan detail ngejelasinnya satu-satu",
+    review: "Seruuu dan detail ngejelasinnya satu-satu.",
   },
   {
     name: "Muh. Falach Achsan Yusuf",
-    review: "Such a great experience mentoring with Kak Kae, one of the best fullstack mentors.",
+    review: "Great mentoring experience, one of the best mentors.",
   },
   {
     name: "Jerry Johnson",
@@ -102,11 +102,11 @@ const testimonials = [
   },
   {
     name: "Rinday Zildjiani Salji",
-    review: "Kak Kae the best",
+    review: "Kak Kae the best.",
   },
   {
     name: "Rischa Nurul Hidayati",
-    review: "Selalu berusaha hadir di setiap mentoring kami dan memberikan saran serta solusi yang sangat membantu. Menjelaskan materi dengan baik dan membuat suasana gmeet menjadi lebih nyaman.",
+    review: "Always present and helpful in every mentoring session.",
   },
 ];
 
